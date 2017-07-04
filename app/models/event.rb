11fbc814b4941d8c78567d0a129378e50c653769
Moboluwarin <kolaogunnaike@gmail.com>
2017-07-04 23:11:02 +0200

    One-to-many:controllers

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,5 +1,13 @@
 class Event < ApplicationRecord
    belongs_to :user
    validates :name, presence: true
+
    validates :description, presence:true, length:{ maximum: 500 }
+   def change
+    change_column :price, :admin, :decimal, default: 0
+    change_column :includes_food, :admin, :boolean, default: false
+    change_column :includes_drinks, :admin, :boolean, default: false
+    change_column :active, :admin, :boolean, default: true
+  end
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.destroy_all
+tolu = User.create(email:"tolu@yahoo.com", password:"1234abcd")
+event = Event.create(name:"baking", description:"a time to bake", location:"Almere 24", price:100,capacity:20,
+  includes_food:true, includes_drinks:true, starts_at:Time.now, ends_at: Date.parse('31-12-2017'), active:true, user:tolu)
